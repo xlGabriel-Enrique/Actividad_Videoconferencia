94f35255d5cf57f9e9ec5d192c156d7989623bf8
Gabriel <xenrique.azds@gmail.com>
2020-05-31 15:34:37 -0500

    Se modifico el archivo .cpp y en bitacora se registraron esos cambios

--- Robalino_Sinchiguano_Gabriel_Enrique.cpp
+++ Robalino_Sinchiguano_Gabriel_Enrique.cpp
@@ -7,4 +7,14 @@ int main()
 
 cout<< "\nMi primer repositorio Local/Remoto";
 
+cout<< "\nUniversidad de las Fuerzas Armadas ESPE";
+
+cout<< "\nEstudiante: Gabriel Enrique Robalino Sinchiguano";
+
+cout<< "\nAsignatura: Programacion Orientada a Objetos";
+
+cout<< "\nTutor: Ing. César Osorio";
+
+cout<< "\nNRC: 8663";
+
 }
